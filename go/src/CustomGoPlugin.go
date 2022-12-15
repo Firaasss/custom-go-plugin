7345e5545a59d21c00767859c9fb04d93f576530
Firaasss <firas@tyk.io>
2022-12-15 12:30:43 -0500

    created key creation function, needs to be unmarshalled correctly

--- go/src/CustomGoPlugin.go
+++ go/src/CustomGoPlugin.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"net/http"
 
 	"github.com/TykTechnologies/tyk/ctx"
@@ -10,14 +12,49 @@ import (
 
 var logger = log.Get()
 
+type Guid struct {
+	Token string
+}
+
+var (
+	policiesToApply = []string{
+		"639a488d6aff8e0001fc6aa6",
+	}
+)
+
 // AddFooBarHeader adds custom "Foo: Bar" header to the request
-func AddFooBarHeader(rw http.ResponseWriter, r *http.Request) {
-	r.Header.Add("Foo", "Bar")
+func KeyCreation(rw http.ResponseWriter, res *http.Response, req *http.Request) {
+	var g Guid
+	b, err := ioutil.ReadAll(req.Body)
+	logger.Info("JSON VALUE: ", b)
+	json.Unmarshal([]byte(b), &g)
+	// err := json.NewDecoder(r.Body).Decode(&g)
+	// google how to unmarshall key value pairs in golang
+	// need to construct struct & parse json into the struct
+
+	// if token exists in temporary redis then create key and g.token not nil
+	// else fail request
+	if err != nil {
+		rw.WriteHeader(http.StatusBadGateway)
+		return
+	}
+
+	session := &user.SessionState{
+		Alias: g.Token,
+		MetaData: map[string]interface{}{
+			"token": g.Token,
+		},
+		KeyID:         g.Token,
+		ApplyPolicies: policiesToApply,
+	}
+	ctx.SetSession(req, session, true)
+
 }
 
 // Custom Auth, applies a rate limit of
 // 2 per 10 given a token of "abc"
 func AuthCheck(rw http.ResponseWriter, r *http.Request) {
+	logger.Info("JSON VALUE: ")
 	token := r.Header.Get("Authorization")
 	if token != "d3fd1a57-94ce-4a36-9dfe-679a8f493b49" && token != "3be61aa4-2490-4637-93b9-105001aa88a5" {
 		rw.WriteHeader(http.StatusUnauthorized)
@@ -26,8 +63,6 @@ func AuthCheck(rw http.ResponseWriter, r *http.Request) {
 
 	session := &user.SessionState{
 		Alias: token,
-		Rate:  2,
-		Per:   10,
 		MetaData: map[string]interface{}{
 			token: token,
 		},
